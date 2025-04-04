cbad242858142f2356acdd4369e3e4ae27d42834
FlyinPancake <FlyinPancake@users.noreply.github.com>
2025-04-04 18:09:14 +0200

    feat(hudson): add images module and update related entities

--- hudson/migration/src/lib.rs
+++ hudson/migration/src/lib.rs
@@ -3,6 +3,7 @@
 pub use sea_orm_migration::prelude::*;
 mod m20220101_000001_users;
 
+mod m20250404_140407_images;
 pub struct Migrator;
 
 #[async_trait::async_trait]
@@ -10,6 +11,7 @@ impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
             Box::new(m20220101_000001_users::Migration),
+            Box::new(m20250404_140407_images::Migration),
             // inject-above (do not remove this comment)
         ]
     }
